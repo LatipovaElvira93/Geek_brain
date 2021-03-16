0448e883e7d154e9ce0d49197bef6b1d93264ff8
Elvira <elyamirsayapova@mail.ru>
2021-03-17 00:16:46 +0500

    first_practic

---
+++ practic_3.py
@@ -0,0 +1,9 @@
+
+
+for i in range(0, 21):
+    if i ==0 or i >= 5:
+        print(str(i) + "процент" + "ов")
+    elif 2 <= i < 5:
+        print(str(i) + "процент" + "а")
+    else:
+        (str(i) + "процент")
